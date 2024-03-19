30dc91e152755322a21dfa446ba98ab9ececa62e
Elwin <elwin.deblauwer@wur.nl>
2024-03-19 15:42:50 +0100

    Add settings and credential-presentation to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { CredentialRequestComponent } from './credential-request/credential-requ
 import { CredentialOverviewComponent } from './credential-overview/credential-overview.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SavevcComponent } from './savevc/savevc.component';
+import { SettingsComponent } from './settings/settings.component';
+import { CredentialPresentationComponent } from './credential-presentation/credential-presentation.component';
 
 export const routes: Routes = 
 [
     {path: 'credentials-request', component: CredentialRequestComponent},
     {path: 'credential-overview', component: CredentialOverviewComponent},
+    {path: 'credential-overview', component: CredentialOverviewComponent},
+    {path: 'credential-presentation', component: CredentialPresentationComponent},
     {path: 'savevc', component: SavevcComponent},
     {path: '', redirectTo: 'credential-overview', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent}
